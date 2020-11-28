37a7b92821859605ae7095d8d92b1b8716f59cf2
Karim977 <karimelkobrossy97@yahoo.com>
2020-11-28 11:07:27 +0200

    Create setup.py

---
+++ distribution_package/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name = 'distributions', 
+      version = '1.0', 
+      description = 'Gaussian distribution',
+      packages = ['distributions'],
+      zip_safe = False)
